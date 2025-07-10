d5721a9336b500d110bc9a491c3e184af7de1d84
eriklidb <eriklidb@student.chalmers.se>
2025-07-10 18:27:44 +0200

    feat(sorting): add insertion sort function

--- sorting/sort.go
+++ sorting/sort.go
@@ -21,7 +21,7 @@ func BubbleSort (arr[]int) {
 }
 func SelectionSort (arr[]int) {
 	size := len(arr)
-	
+
 	for i := 0; i < size - 1; i++ {
 		min_index := i
 		
@@ -41,4 +41,21 @@ func SelectionSort (arr[]int) {
 	}
 }
 
-func InsertionSort (arr[]int) {}
+func InsertionSort (arr[]int) {
+
+	size := len(arr)
+	for i := 0; i < size - 1; i++ {
+		j := i
+
+		for j > 0 && arr[j] < arr[j - 1] {
+			arr[j], arr[j - 1] = arr[j - 1], arr[j]
+			j--
+		}
+	} 
+
+	fmt.Println("Newly sorted array")
+	
+	for i := 0; i < size - 1; i++{
+		fmt.Printf("%d, ", arr[i])
+	}
+}
