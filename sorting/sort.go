767246473afa1d8fb818be90fd50de7d967aac7b
eriklidb <eriklidb@student.chalmers.se>
2025-07-10 18:01:50 +0200

    feat(sorting): add selection sort function

--- sorting/sort.go
+++ sorting/sort.go
@@ -2,22 +2,43 @@ package sort
 
 import "fmt"
 
-func BubbleSort (arr []int) {
-	
-
+func BubbleSort (arr[]int) {
 	size := len(arr)
 
-	for i := 0; i < size - 1; i++{
-		for j:=0; j < size - i - 1; j++{
-			if(arr[j] > arr[j+1]){
+	for i := 0; i < size - 1; i++ {
+		for j := 0; j < size - i - 1; j++{
+			if(arr[j] > arr[j + 1]){
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
 
 	fmt.Println("Newly sorted array")
-
-	for i := 0; i < size; i++{
+	
+	for i:=0; i < size - 1; i++{
 		fmt.Printf("%d, ", arr[i])
 	}
 }
+func SelectionSort (arr[]int) {
+	size := len(arr)
+	
+	for i := 0; i < size - 1; i++ {
+		min_index := i
+		
+		for j := i + 1; j < size; j++ {
+			if(arr[j] < arr[min_index]){
+				min_index = j
+			}
+		}
+
+		arr[i], arr[min_index] = arr[min_index], arr[i]
+	}
+
+	fmt.Println("Newly sorted array")
+	
+	for i:=0; i < size - 1; i++{
+		fmt.Printf("%d, ", arr[i])
+	}
+}
+
+func InsertionSort (arr[]int) {}
