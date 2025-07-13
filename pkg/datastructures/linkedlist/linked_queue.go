7950134d9c55de03a9e6800c6c828d3308349833
eriklidb <eriklidb@student.chalmers.se>
2025-07-13 13:33:04 +0200

    feat: implementation of single linked list and linked queue

---
+++ pkg/datastructures/linkedlist/linked_queue.go
@@ -0,0 +1,59 @@
+package linkedlist
+
+import "errors"
+
+type LinkedQueue struct {
+	front *Node
+	rear *Node
+	size int
+}
+
+func NewLinkedQueue() *LinkedQueue {
+	return &LinkedQueue{}
+}
+
+func (lq *LinkedQueue) Size() int {
+	return lq.size
+}
+
+func (lq *LinkedQueue) IsEmpty() bool {
+	return lq.size == 0
+}
+
+func (lq *LinkedQueue) Enqueue(x any) {
+	newNode := &Node{value: x, next: nil}
+	if lq.IsEmpty() {
+		lq.front = newNode
+	} else {
+		lq.rear.next = newNode
+	}
+	
+	lq.rear = newNode
+	lq.size++
+}
+
+func (lq *LinkedQueue) Dequeue() (any, error){
+	if lq.IsEmpty() {
+		return nil, errors.New("cannot dequeue an empty queue")
+	}
+
+	dequeuedNode := lq.front
+	lq.front = dequeuedNode.next
+	dequeuedNode.next = nil
+
+	lq.size--
+
+	if lq.IsEmpty() {
+		lq.rear = nil
+	} 
+	return dequeuedNode, nil
+}
+
+func (lq *LinkedQueue) Peek() (any, error) {
+
+	if lq.IsEmpty() {
+		return nil, errors.New("cannot peek an empty queue")
+	}
+
+	return lq.front.value, nil
+}
---
+++ pkg/datastructures/linkedlist/singly_linked_list.go
@@ -0,0 +1,56 @@
+package linkedlist
+
+import "errors"
+
+type Node struct {
+	value any
+	next *Node
+}
+
+type SinglyLinkedList struct {
+	head *Node
+	size int
+}
+
+func NewSinglyLinkedList() *SinglyLinkedList {
+	return &SinglyLinkedList{}
+}
+
+func (sl *SinglyLinkedList) Size() int {
+	return sl.size
+}
+
+func (sl *SinglyLinkedList) IsEmpty() bool {
+	return sl.size == 0
+}
+
+func (sl *SinglyLinkedList) Push(x any) {
+	new_node := &Node{
+		value: x,
+		next: sl.head,
+	}
+
+	sl.head = new_node
+	sl.size++
+}
+
+func (sl *SinglyLinkedList) Pop() (any, error) {
+	if sl.IsEmpty() {
+		return nil, errors.New("cannot pop an element of an empty list")
+	}
+	popped_node := sl.head
+	popped_node.next = nil
+	sl.head = sl.head.next
+	sl.size--
+
+	return popped_node.value, nil
+}
+
+func (sl *SinglyLinkedList) Peek() (any, error) {
+
+	if sl.IsEmpty() {
+		return nil, errors.New("cannot peek an empty list")
+	}
+
+	return sl.head.value, nil
+}
