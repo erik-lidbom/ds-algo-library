d64da6a77a7ba171c5a9edc6336367749a1fc02e
eriklidb <eriklidb@student.chalmers.se>
2025-07-14 05:13:14 +0200

    chore: add main file

--- main.go
+++ main.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"os"
-	"data-structures/linked_list")
-
-type Single_Node struct  {
-	value int
-	next *Single_Node
-}
-
-type Linked_List struct {
-	head *Single_Node
-	size int
-}
+import "fmt"
 
 func main(){
-
-    myList := &linked_list.Linked_List{} 
-    myList.Prepend(10)
-    myList.Append(20)
-	
-	os.Exit(0)
+	fmt.Println("ozzy")
 }
 
