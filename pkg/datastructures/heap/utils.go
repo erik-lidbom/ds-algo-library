7617c43ebca26540c0e6a385fac3a70f9a8de43b
eriklidb <eriklidb@student.chalmers.se>
2025-07-20 22:52:27 +0200

    chore: move auxiliary functiosn into utils folder

---
+++ pkg/datastructures/heap/utils.go
@@ -0,0 +1,17 @@
+package heap
+
+func isLeaf(pos, size int) bool {
+	return pos >= size / 2
+}
+
+func getLeftChildIndex(pos int) int {
+	return 2 * pos + 1
+}
+
+func getRightChildIndex (pos int) int {
+	return 2 * pos + 2
+}
+
+func getParent(pos int) int {
+	return int((pos - 1) / 2)
+}
