9fc618a897516c1d420e8ce2e0074b6ab7b52356
eriklidb <eriklidb@student.chalmers.se>
2025-07-21 21:00:48 +0200

    fix: remove nil assignment
    This nil assignment caused a panic error

--- pkg/datastructures/linkedlist/linked_stack.go
+++ pkg/datastructures/linkedlist/linked_stack.go
@@ -34,7 +34,6 @@ func (sl *SinglyLinkedList) Pop() (any, error) {
 		return nil, errors.New("cannot pop an element of an empty list")
 	}
 	popped_node := sl.head
-	popped_node.next = nil
 	sl.head = sl.head.next
 	sl.size--
 
