cb35026952b4d70be6d2093bf3809eddef830bf5
eriklidb <eriklidb@student.chalmers.se>
2025-07-23 16:26:23 +0200

    chore: improve complexity of ListSet data structure
    Added binary search to the Contain method which changed the complexity of Contain method from O(N) to O(log N).
    
    Since binary search requires a sorted list, I needed to adjust the Add method to always insert a new value at the correct index.

--- pkg/datastructures/sets/list_set.go
+++ pkg/datastructures/sets/list_set.go
@@ -3,11 +3,8 @@ package sets
 import (
 	"cmp"
 	"ds-algorithms/pkg/datastructures/array"
-	"errors"
 	"fmt"
 )
-
-// TODO --> Add binary search for contain method. This requires that the ListSet always is sorted so logic has to be added for Add and Removal.
 
 type ListSet[T cmp.Ordered] struct {
 	arr *array.ArrayList[T]
@@ -27,44 +24,77 @@ func (ls *ListSet[T]) IsEmpty() bool {
 }
 
 func (ls *ListSet[T]) Add(elem T) error {
-	containVal := ls.Contains(elem)
+	duplicatedVal, index := ls.findInsertionPoint(elem)
 
-	if containVal {
-		return fmt.Errorf("cannot add element to set: value %v already exists\n", containVal)
+	if duplicatedVal {
+		return fmt.Errorf("cannot add element to set: value %v already exists\n", duplicatedVal)
 	}
 
-	ls.arr.Add(ls.size, elem)
+	ls.arr.Add(index, elem)
 	ls.size++
 	return nil
 }
 
 func (ls *ListSet[T]) Remove(elem T) (T, error) {
 	var zero T
-	for i := 0; i < ls.size - 1; i++ {
-		currVal, err := ls.arr.Get(i)
-		if err != nil {
-			return zero, errors.New("cannot retrieve value from list")
-		}
 
-		if elem == currVal {
-			ls.arr.Remove(i)
-			return elem, nil
-		}
+	duplicatedVal, index := ls.binary_search(elem)
+	
+	if duplicatedVal {
+		ls.arr.Remove(index)
+		ls.size--
+		return elem, nil
 	}
+
 	return zero, fmt.Errorf("element %v not found in the set", elem)
 }
 
+
 func (ls *ListSet[T]) Contains(elem T) bool {
+	res, _ := ls.binary_search(elem)
+	return res
+}
 
-	for i := 0; i < ls.size - 1; i++ {
-		currVal, err := ls.arr.Get(i)
-		if err != nil {
-			return false
-		}
+func (ls *ListSet[T]) binary_search(elem T) (bool, int) {
+	
+	leftIndex := 0
+	rightIndex := ls.size - 1
 
+	for leftIndex <= rightIndex {
+		midIndex := leftIndex + (rightIndex - leftIndex) / 2
+		currVal, _ := ls.arr.Get(midIndex)
 		if elem == currVal {
-			return true
+		return true, midIndex
+		} else if elem < currVal {
+			rightIndex = midIndex - 1
+		} else { 
+			leftIndex = midIndex + 1
 		}
 	}
-	return false
+	return false, -1
+}
+
+func (ls *ListSet[T]) findInsertionPoint(elem T) (bool, int) {
+    leftIndex := 0
+    rightIndex := ls.size - 1 
+
+    for leftIndex < rightIndex { 
+        mid := leftIndex + (rightIndex - leftIndex) / 2
+        currVal, _ := ls.arr.Get(mid)
+
+        if currVal < elem {
+            leftIndex = mid + 1
+        } else {
+            rightIndex = mid   
+        }
+    }
+  
+    if leftIndex < ls.size - 1 {
+        lowerBoundVal, _ := ls.arr.Get(leftIndex)
+        if lowerBoundVal == elem {
+            return true, leftIndex
+        }
+    }
+
+    return false, leftIndex
 }
