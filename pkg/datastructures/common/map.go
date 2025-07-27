e8f52b06a9b1cb715e5e69da8e822d50c5170cc8
eriklidb <eriklidb@student.chalmers.se>
2025-07-27 03:13:59 +0200

    feat: add interface for sorted map and set

--- pkg/datastructures/common/map.go
+++ pkg/datastructures/common/map.go
@@ -1,9 +1,22 @@
 package common
 
+import "ds-algorithms/pkg/datastructures/array"
+
 type Map[K comparable, V any] interface {
 	Collection
 	Put(key K, value V)
 	Get(key K) (V, error)
 	Remove(key K) (V, error)
 	ContainsKey(key K) bool
+}
+
+type SortedMap[K comparable, V any] interface {
+	Map[K, V]
+	FirstKey() (K, error)
+	LastKey() (K, error)
+	FloorKey(key K) (K, error)
+	CeilingKey(key K) (K, error)
+	LowerKey(key K) (K, error)
+	HigherKey(key K) (K, error)
+	KeysBetween(key1, key2 K) *array.ArrayList[K]
 }
--- pkg/datastructures/common/set.go
+++ pkg/datastructures/common/set.go
@@ -1,8 +1,24 @@
 package common
 
-type Set[T any] interface {
+import (
+	"cmp"
+	"ds-algorithms/pkg/datastructures/array"
+)
+
+type Set[T comparable] interface {
 	Collection
 	Add(elem T) error
 	Remove(elem T) (T, error)
 	Contains(elem T) bool
+}
+
+type SortedSet[T cmp.Ordered] interface {
+	Set[T]
+	First() (T, error)
+	Last() (T, error)
+	Floor(x T) (T, error)
+	Ceiling(x T) (T, error)
+	Lower(x T) (T, error)
+	Higher(x T) (T, error)
+	Between(x, y T) *array.ArrayList[T]
 }
