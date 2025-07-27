56140eefba3c1090c609554bc67b4cdddecd3ea2
eriklidb <eriklidb@student.chalmers.se>
2025-07-27 03:14:18 +0200

    feat: implement SortedMap and SortedSet interface

--- pkg/datastructures/maps/sorted_list_map.go
+++ pkg/datastructures/maps/sorted_list_map.go
@@ -74,3 +74,145 @@ func (slm *SortedListMap[K, V]) ContainsKey(key K) bool {
 	res, _ := search.BinarySearchArrayList(slm.keys, key)
 	return res
 }
+
+func (slm *SortedListMap[K, V]) FirstKey() (K, error) {
+	var zero K
+
+	if slm.IsEmpty() {
+		return zero, fmt.Errorf("map is empty")
+	}
+
+	key, err := slm.keys.Get(0)
+	if err != nil {
+		return zero, fmt.Errorf("internal error: failed to retrieve first key: %w", err)
+	}
+
+	return key, nil
+}
+
+func (slm *SortedListMap[K, V]) LastKey() (K, error) {
+	var zero K
+
+	if slm.IsEmpty() {
+		return zero, fmt.Errorf("map is empty")
+	}
+
+	key, err := slm.keys.Get(slm.Size() - 1)
+	if err != nil {
+		return zero, fmt.Errorf("internal error: failed to retrieve last key: %w", err)
+	}
+
+	return key, nil
+}
+
+func (slm *SortedListMap[K, V]) FloorKey(key K) (K, error) {
+	var zero K
+
+	if slm.IsEmpty() {
+		return zero, fmt.Errorf("map is empty")
+	}
+
+	found, index := search.FindInsertionPoint(slm.keys, key)
+
+	if found {
+		floorKey, err := slm.keys.Get(index)
+		if err != nil {
+			return zero, fmt.Errorf("internal error: failed to retrieve floorKey at index %d: %w", index, err)
+		}
+
+		return floorKey, nil
+	}
+
+	if index == 0 {
+		return zero, fmt.Errorf("no element <= %v found in map", key)
+	}
+
+	floorKey, err := slm.keys.Get(index - 1)
+	if err != nil {
+        return zero, fmt.Errorf("internal error: failed to retrieve floorKey at index %d-1: %w", index, err)
+    }
+	return floorKey, nil
+}
+
+func (slm *SortedListMap[K, V]) CeilingKey(key K) (K, error) {
+	var zero K
+
+	if slm.IsEmpty() {
+		return zero, fmt.Errorf("map is empty")
+	}
+
+	_, index := search.FindInsertionPoint(slm.keys, key)
+
+	if index == slm.Size() {
+		return zero, fmt.Errorf("no key >= %v found in map", key)
+	}
+
+	ceilingKey, err := slm.keys.Get(index)
+	if err != nil {
+        return zero, fmt.Errorf("internal error: failed to retrieve ceilingKey at index %d: %w", index, err)
+    }
+
+	return ceilingKey, nil
+}
+
+func (slm *SortedListMap[K, V]) LowerKey(key K) (K, error) {
+	var zero K
+
+	if slm.IsEmpty() {
+		return zero, fmt.Errorf("map is empty")
+	}
+
+	_, index := search.FindInsertionPoint(slm.keys, key)
+	if index == 0 {
+		return zero, fmt.Errorf("no key < %v found in map", key)
+	}
+
+	lowerKey, err := slm.keys.Get(index - 1)
+	if err != nil {
+		return zero, fmt.Errorf("internal error: failed to retrieve lower key at index %d: %w", index-1, err)
+	}
+
+	return lowerKey, nil
+}
+
+func (slm *SortedListMap[K, V]) HigherKey(key K) (K, error) {
+	var zero K
+
+	if slm.IsEmpty() {
+		return zero, fmt.Errorf("map is empty")
+	}
+
+	_, index := search.FindUpperBound(slm.keys, key)
+
+	if index == slm.Size() {
+		return zero, fmt.Errorf("no element > %v found in map", key)
+	}
+
+	higherKey, err := slm.keys.Get(index)
+	if err != nil {
+		return zero, fmt.Errorf("internal error: failed to retrieve higherKey at index %d: %w", index-1, err)
+	}
+
+	return higherKey, nil
+}
+
+func (slm *SortedListMap[K, V]) KeysBetween(key1, key2 K) *array.ArrayList[K] {
+	arr := array.NewArrayList[K]()
+
+	if slm.IsEmpty() {
+		return arr
+	}
+
+	_, startIndex := search.FindInsertionPoint(slm.keys, key1)
+	_, endIndex := search.FindUpperBound(slm.keys, key2)
+
+	if startIndex >= slm.Size() || startIndex >= endIndex {
+		return arr
+	}
+
+	for i := startIndex; startIndex < endIndex; i++ {
+		currVal, _ := slm.keys.Get(i)
+		arr.Add(i, currVal)
+	}
+	return arr
+}
--- pkg/datastructures/sets/list_set.go
+++ pkg/datastructures/sets/list_set.go
@@ -55,3 +55,140 @@ func (ls *ListSet[T]) Contains(elem T) bool {
 	res, _ := search.BinarySearchArrayList(ls.arr, elem)
 	return res
 }
+
+func (ls *ListSet[T]) First() (T, error) {
+	var zero T
+	if ls.IsEmpty() {
+		return zero, fmt.Errorf("set is empty")
+	}
+
+	first, err := ls.arr.Get(0)
+	if err != nil {
+		return zero, fmt.Errorf("failed to retrieve first element: %w", err)
+	}
+
+	return first, nil
+}
+
+func (ls *ListSet[T]) Last() (T, error) {
+	var zero T
+	if ls.IsEmpty() {
+		return zero, fmt.Errorf("set is empty")
+	}
+
+	last, err := ls.arr.Get(ls.Size() - 1)
+	if err != nil {
+		return zero, fmt.Errorf("failed to retrieve first element: %w", err)
+	}
+
+	return last, nil
+}
+
+func (ls *ListSet[T]) Floor(x T) (T, error) {
+	var zero T
+	if ls.IsEmpty() {
+		return zero, fmt.Errorf("set is empty")
+	}
+
+	found, index := search.FindInsertionPoint(ls.arr, x)
+
+	if found {
+		floor, err := ls.arr.Get(index)
+        if err != nil {
+            return zero, fmt.Errorf("internal error: failed to retrieve floor element at index %d: %w", index, err)
+        }
+        return floor, nil
+	}
+
+	if index == 0 {
+		return zero, fmt.Errorf("no element <= %v found in set", x)
+	}
+
+	floor, err := ls.arr.Get(index - 1)
+    if err != nil {
+        return zero, fmt.Errorf("internal error: failed to retrieve floor element at index %d-1: %w", index, err)
+    }
+	return floor, nil
+}
+
+func (ls *ListSet[T]) Ceiling(x T) (T, error) {
+	var zero T
+	if ls.IsEmpty() {
+		return zero, fmt.Errorf("set is empty")
+	}
+
+	_, index := search.FindInsertionPoint(ls.arr, x)
+
+	if index == ls.Size() {
+		return zero, fmt.Errorf("no element >= %v found in set", x)
+	}
+
+	ceiling, err := ls.arr.Get(index)
+	 if err != nil {
+        return zero, fmt.Errorf("internal error: failed to retrieve ceiling element at index %d: %w", index, err)
+    }
+
+	return ceiling, nil
+}
+func (ls *ListSet[T]) Lower(x T) (T, error) {
+	var zero T
+	if ls.IsEmpty() {
+		return zero, fmt.Errorf("set is empty")
+	}
+
+	_, index := search.FindInsertionPoint(ls.arr, x)
+
+	if index == 0 {
+		return zero, fmt.Errorf("no element < %v found in set", x)
+	}
+
+	lower, err := ls.arr.Get(index - 1)
+	if err != nil {
+		return zero, fmt.Errorf("internal error: failed to retrieve lower element at index %d: %w", index-1, err)
+	}
+
+	return lower, nil
+}
+
+func (ls *ListSet[T]) Higher(x T) (T, error) {
+	var zero T
+	if ls.IsEmpty() {
+		return zero, fmt.Errorf("set is empty")
+	}
+
+	_, index := search.FindUpperBound(ls.arr, x)
+
+	if index == ls.Size() {
+		return zero, fmt.Errorf("no element > %v found in set", x)
+	}
+
+	higher, err := ls.arr.Get(index)
+	if err != nil {
+		return zero, fmt.Errorf("internal error: failed to retrieve higher element at index %d: %w", index-1, err)
+	}
+
+	return higher, nil
+}
+
+func (ls *ListSet[T]) Between(x, y T) *array.ArrayList[T]{
+
+	arr := array.NewArrayList[T]()
+
+	if ls.IsEmpty() {
+		return arr
+	}
+
+	_, startIndex := search.FindInsertionPoint(ls.arr, x)
+	_, endIndex := search.FindUpperBound(ls.arr, y)
+
+	if startIndex >= ls.Size() || startIndex >= endIndex {
+		return  arr
+	}
+
+	for i := startIndex; i < endIndex; i++ {
+		currVal, _ := ls.arr.Get(i)
+		arr.Add(i, currVal)
+	}
+
+	return arr
+}
